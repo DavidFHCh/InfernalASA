1b1f1e9691080cf904362fc44bf194b3a94103c1
Diego Carrillo <dailexgo@gmail.com>
2017-10-01 15:22:47 -0500

    Iniciando el armado del AST

---
+++ ast.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef enum _ops {
+  ADD, SUBST, MULT, DIV, LT, LE, EQ
+} ops;
+
+typedef struct _exp {
+  enum {
+    INT,  STR,  BOOL, BIN_OP
+  } tag;
+  
+  union {
+    int int_expr;
+    int bool_expr;
+    char* str_expr;
+    struct {
+      ops op;
+      struct _exp* left_expr; 
+      struct _exp* right_expr;
+    } binop_expr;
+  } op;
+} exp;
+
+// Constructor de expresiones de tipo INT
+exp* make_int_exp(int value) {
+  exp* e = (exp*) malloc(sizeof(exp));
+  e->tag = INT;
+  e->op.int_expr = value;
+  return e;
+}
+
+exp* make_bool_exp(int value) {
+  exp* e = (exp*) malloc(sizeof(exp));
+  e->tag = BOOL;
+  e->op.bool_expr = value;
+  return e;
+}
+
+// Constructor de expresiones de tipo STR
+exp* make_str_exp(char* value) {
+  exp* e = (exp*) malloc(sizeof(exp));
+  e->tag = STR;
+  e->op.str_expr = value;
+  return e;
+}
+
+//void free_exp(exp* e) {
+//  free(e);
+//}
+
+int main() {
+  exp* e = make_int_exp(69);
+  printf("Expresión e con etiqueta %d y valor %d \n", e->tag, e->op.int_expr);
+  exp* e2 = make_str_exp("holi");
+  printf("Expresión e2 con etiqueta %d y valor %s \n", e2->tag, e2->op.str_expr);
+  exp* e3 = make_bool_exp(0);
+  printf("Expresion e3 con etiqueta %d y valor %d \n", e3->tag, e3->op.bool_expr);
+  free(e);
+  free(e2);
+  free(e3);
+}
